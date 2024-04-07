06be93a3da225ac95e90b9e92a1c954af0368a2d
Elena Bairamova <elshishkanova@yandex.ru>
2024-04-07 19:25:27 +0300

    add page object

---
+++ src/test/java/org/ibs/tests/BaseTest.java
@@ -0,0 +1,40 @@
+package org.ibs.tests;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.concurrent.TimeUnit;
+
+
+public class BaseTest {
+    public static WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://localhost:8080");
+        driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+    }
+
+    @AfterAll
+    static void afterAll() {
+        driver.close();
+        driver.quit();
+    }
+
+    public final String exoticFruitName = "Папайя";
+    public final String notExoticFruitName = "Банан";
+    public final String exoticVegetableName = "Кивано";
+    public final String notExoticVegetableName = "Огурец";
+
+    public final String exoticFruitNameResult = "5 Папайя Фрукт true";
+    public final String notExoticFruitNameResult = "5 Банан Фрукт false";
+    public final String exoticVegetableNameResult = "5 Кивано Овощ true";
+    public final String notExoticVegetableNameResult = "5 Огурец Овощ false";
+
+
+}
