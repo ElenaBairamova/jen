c24589b4b02ed8af4e4af70ae40bf08a7ccb2715
Elena Bairamova <elshishkanova@yandex.ru>
2024-04-10 22:38:43 +0300

    add parametr

--- src/test/java/org/ibs/steps/StepAddGoods.java
+++ src/test/java/org/ibs/steps/StepAddGoods.java
@@ -1,4 +1,5 @@
 package org.ibs.steps;
+
 import io.cucumber.java.ru.Дано;
 import io.cucumber.java.ru.И;
 import io.cucumber.java.ru.Когда;
@@ -13,15 +14,22 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
+import static org.openqa.selenium.remote.ErrorCodes.TIMEOUT;
 
-public class StepAddGoods  {
+
+public class StepAddGoods {
     public static WebDriver driver;
+
     @Дано("открыт стенд")
     public void openStand() {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
@@ -32,54 +40,42 @@ public class StepAddGoods  {
         driver.get("http://localhost:8080");
 
     }
-    @Когда("Пользователь нажимает на выпадающий список Песочница")
-    public void clickSandboxButton() {
+
+    @Когда("Пользователь нажимает на выпадающий список {string}")
+    public void clickSandboxButton(String string) {
         WebElement sandboxButton = driver.findElement(By.xpath("//a [@id='navbarDropdown']"));
-       sandboxButton.click();
+        sandboxButton.click();
     }
-    @И("Пользователь выбирает в выпадающем списке пункт Товары")
-    public void clickGoodsButton() {
+
+    @И("Пользователь выбирает в выпадающем списке пункт {string}")
+    public void clickGoodsButton(String string) {
         WebElement goodsButton = driver.findElement(By.xpath("//a [@href='/food']"));
         goodsButton.click();
     }
-    @И("Пользователь нажимает на кнопку Добавить")
-    public void clickAddButton() {
+
+    @И("Пользователь нажимает на кнопку {string}")
+    public void clickAddButton(String string) {
         WebElement addButton = driver.findElement(By.xpath("//button[text()='Добавить']"));
         addButton.click();
     }
-    @И("Пользователь вводит наименование экзотического фрукта")
-    public void NameGoodExoticFruit() {
+
+    @И("Пользователь вводит наименование {string}")
+    public void NameGoodExoticFruit(String name) {
         WebElement nameGood = driver.findElement(By.xpath("//input[@id='name']"));
-        nameGood.sendKeys("Папайя");
+        nameGood.sendKeys(name);
     }
-    @И("Пользователь вводит наименование экзотического овоща")
-    public void NameGoodExoticVegetable() {
-        WebElement nameGood = driver.findElement(By.xpath("//input[@id='name']"));
-        nameGood.sendKeys("Кивано");
-    }
-    @И("Пользователь вводит наименование фрукта")
-    public void NameGoodNotExoticFruit() {
-        WebElement nameGood = driver.findElement(By.xpath("//input[@id='name']"));
-        nameGood.sendKeys("Банан");
-    }
-    @И("Пользователь вводит наименование овоща")
-    public void NameGoodNotExoticVegetable() {
-        WebElement nameGood = driver.findElement(By.xpath("//input[@id='name']"));
-        nameGood.sendKeys("Огурец");
-    }
-    @И("Пользователь нажимает на тип и выбирает в выпадающем списке тип Фрукт")
-    public void clickDropdownTypeFruit() {
-        WebElement goodType = driver.findElement(By.id("type"));
-        goodType.click();
-        WebElement typeFruit = driver.findElement(By.xpath("//option[@value='FRUIT']"));
-        typeFruit.click();
-    }
-    @И("Пользователь нажимает на тип и выбирает в выпадающем списке тип Овош")
-    public void clickDropdownTypeVegetable() {
-        WebElement goodType = driver.findElement(By.id("type"));
-        goodType.click();
-        WebElement typeFruit = driver.findElement(By.xpath("//option[@value='VEGETABLE']"));
-        typeFruit.click();
+
+    @И("Пользователь нажимает на тип и выбирает в выпадающем списке тип {string}")
+    public void clickDropdownTypeFruit(String goodType) {
+        WebElement goodType1 = driver.findElement(By.id("type"));
+        goodType1.click();
+        if (Objects.equals(goodType, "Овощ")) {
+            WebElement selectedFoodType = driver.findElement(By.xpath("//option[@value='VEGETABLE']"));
+            selectedFoodType.click();
+        } else if (Objects.equals(goodType, "Фрукт")) {
+            WebElement selectedFoodType = driver.findElement(By.xpath("//option[@value='FRUIT']"));
+            selectedFoodType.click();
+        }
     }
 
     @И("Пользователь выбирает чек-бокс Экзотический")
@@ -88,19 +84,19 @@ public class StepAddGoods  {
         checkBoxExotic.click();
     }
 
-    @И("Пользователь нажимает кнопку Сохранить")
-    public void clickSaveButton() {
+    @И("Пользователь нажимает кнопку {string}")
+    public void clickSaveButton(String string) {
         WebElement saveButton = driver.findElement(By.id("save"));
         saveButton.click();
 
     }
+
     @Тогда("Проверяем, что в последней строке таблицы Товары отображаются введенные данные экзотического фрукта")
     public void assertResultExoticFruit() {
 
 
         List<String> listExoticFruit = Arrays.asList(
                 "5", "Папайя", "Фрукт", "true");
-
 
         List<WebElement> lastRowInTableOfGoods = driver.findElements(By.xpath(
                 "//tr[5]/*"));
@@ -139,6 +135,7 @@ public class StepAddGoods  {
                 lastRowInTableOfGoodsStr,
                 "Не совпадают элементы в таблице товаров");
     }
+
     @Тогда("Проверяем, что в последней строке таблицы Товары отображаются введенные данные экзотического овоща")
     public void assertResultExoticVegetable() {
 
@@ -161,6 +158,7 @@ public class StepAddGoods  {
                 lastRowInTableOfGoodsStr,
                 "Не совпадают элементы в таблице товаров");
     }
+
     @Тогда("Проверяем, что в последней строке таблицы Товары отображаются введенные данные овоща")
     public void assertResultFruit() {
 
@@ -183,12 +181,14 @@ public class StepAddGoods  {
                 lastRowInTableOfGoodsStr,
                 "Не совпадают элементы в таблице товаров");
     }
+
     @И("Пользователь выбирает в выпадающем списке пункт Сброс данных")
     public void clickDeleteData() {
-      WebElement deleteDataButton = driver.findElement(By.xpath("//a [@id='reset']"));
-      deleteDataButton.click();
+        WebElement deleteDataButton = driver.findElement(By.xpath("//a [@id='reset']"));
+        deleteDataButton.click();
     }
-    @Тогда ("Закрываем соединение")
+
+    @Тогда("Закрываем соединение")
     public void finish() {
         driver.close();
         driver.quit();
